b05c054d7a0602f088fb3f844cb335bdfd84a922
nishaalnaseer <nishaalnaseer@users.noreply.github.com>
2022-12-01 16:13:53 +0500

    indexing() linux file system support

--- DiscordBotServer/main.py
+++ DiscordBotServer/main.py
@@ -40,13 +40,20 @@ def indexing(path):
     index = {}
     songs = []
 
+    if os.name == "nt":
+        #  for windows
+        slash = '\\'
+    else:
+        #  for linux
+        slash = '/'
+
     def search_folders(folder_path):
         items = os.listdir(folder_path)
         folders = []
         num = 0
         for ii in range(len(items)):
             file = items[ii]
-            file_path = f"{folder_path}\\{file}"
+            file_path = f"{folder_path}{slash}{file}"
             if os.path.isdir(file_path):
                 folders.append(file_path)
                 continue
@@ -449,4 +456,5 @@ SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 CLIENTS = []
 
 if __name__ == '__main__':
-    Bot()
+    # Bot()
+    print(indexing(config["root_music_folder"]))
